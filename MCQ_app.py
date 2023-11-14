100588c6c828307c9ce57914add0a2f105f6edcd
vamsi <vamsikumar3u@gmail.com>
2023-11-14 23:54:59 +0530

    MCQ

--- MCQ_app.py
+++ MCQ_app.py
@@ -20,13 +20,13 @@ def predict():
     course_name = request.form['course_name']
     btech_branch = request.form['btech_branch']
     QA_difficulty = request.form['QA_difficulty']
-    text="generate only"+ QA_count+QA_difficulty+"Multi choice questions on "+course_name+"for"+btech_branch+"branch."
+    text="generate only"+ QA_count+QA_difficulty+"Multi choice questions on "+course_name+"for"+btech_branch+"branch starting with *Question*."
     palm.configure(api_key='AIzaSyCpta0zYFZSLw7imatVqW-exaviTfMIqu0')
     response = palm.generate_text(prompt=text)
     questions_string = response.result
-    answers=palm.generate_text(prompt='generate only options for:'+response.result)
+    answers=palm.generate_text(prompt='generate only options for:'+questions_string)
     answers_list=[i for i in answers.result.split('\n')]
-    questions = [question.strip() for question in questions_string.split("\n") if question]
+    questions = [question.strip() for question in questions_string.split("Question") if question]
     questions_list=[]
     # answers_list=[]
     y=''
